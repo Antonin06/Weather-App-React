dd4793a9549a6058381d14635f9ebdf9821362eb
Antonin06 <avon.antonin@gmai.com>
2020-08-07 11:26:47 +0200

    adding component

---
+++ src/Component/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import '../Weather.css';
+
+
+const Header = () => {
+  return(
+
+<header class="App-header"><img src="../logo_transparent.png" class="App-logo" alt="logo"></img></header>
+
+)}
+
+export default Header;
---
+++ src/Weather.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import './Weather.css';
+
+const Weather = () => {
+  return (
+
+    <div class="row">
+      <div class="col s12 m6 push-m3">
+        <div class="weather card blue-grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">Lyon</span>
+            <p><img src="../sun.svg"></img></p>
+            <span class="temperature">15°</span>
+          <div class="wind">Vent 1km/h (360°)</div>
+          </div>
+          <div class="card-action">
+            <a href="#">Thursday</a>
+            <a href="#">Friday</a>
+            <a href="#">Saturday</a>
+            <a href="#">Sunday</a>
+            <a href="#">Monday</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  )}
+  export default Weather;
